76f62dfa6b82d721bb9676c61c7ecb8d42b0c692
Martin Matovu <matovu.synergy@gmail.com>
2019-01-24 11:00:12 +0300

    Added a model mixin inheriting features from (utils.models)

--- myapp1/models.py
+++ myapp1/models.py
@@ -1,3 +1,29 @@
-from django.db import models
 
-# Create your models here.
+# myapps1/models.py
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
+from django.utils.encoding import python_2_unicode_compatible
+from utils.models import UrlMixin
+from utils.models import CreationModificationMixin
+from utils.models import MetaTagsMixin
+
+
+@python_2_unicode_compatible
+class Idea(UrlMixin, CreationModificationMixin, MetaTagsMixin):
+	title = models.CharField(_("Title"), max_length=200)
+	content = models.TextField(_("Content"))
+
+	class Meta:
+		verbose_name = _("Idea")
+		verbose_name_plural = _("Ideas")
+
+	def __str__(self):
+		return self.title
+		
+	get_url_path(self):
+		return reverse("idea_details", kwargs={ "idea_id": str(self.pk),} )
+
+
