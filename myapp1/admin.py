299cf6bfac711f5b19fada4d266fb0a69ecaf089
Martin Matovu <matovu.synergy@gmail.com>
2019-02-01 01:44:56 +0300

    Registered models in the admin.py

--- myapp1/admin.py
+++ myapp1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, Idea, Like
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Idea)
+admin.site.register(Like)
