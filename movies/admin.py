71e97c771548776d61fff8f3df8c5feaaaad1e8c
Martin Matovu <matovu.synergy@gmail.com>
2019-02-26 21:14:40 +0300

    registered movies.models with admin to enabld data entry

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Actor, Director, Genre, Movie
 
-# Register your models here.
+
+admin.site.register(Actor)
+admin.site.register(Director)
+admin.site.register(Genre)
+admin.site.register(Movie)
