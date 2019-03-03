1ea21642cf15921467566a71a5bf1f45d9a26523
Martin Matovu <matovu.synergy@gmail.com>
2019-03-03 09:15:20 +0300

    Edited to improve code readability

--- quotes/forms.py
+++ quotes/forms.py
@@ -12,62 +12,62 @@ from quotes.models import InspirationalQuote
 
 
 class InspirationalQuoteForm(forms.ModelForm):
-	picture_path = forms.CharField(
-		max_length=255,
-		widget=forms.HiddenInput(),
-		required=False,
-	)
-	delete_picture = forms.BooleanField(
-		widget=forms.HiddenInput(),
-		required=False,
-	)
+    picture_path = forms.CharField(
+        max_length=255,
+        widget=forms.HiddenInput(),
+        required=False,
+    )
+    delete_picture = forms.BooleanField(
+        widget=forms.HiddenInput(),
+        required=False,
+    )
 
-	class Meta:
-		model = InspirationalQuote
-		fields = ["author", "quote", "picture", "language"]
+    class Meta:
+        model = InspirationalQuote
+        fields = ["author", "quote", "picture", "language"]
 
-	def __init__(self, *args, **kwargs):
-		super(InspirationalQuoteForm, self). \
-			__init__(*args, **kwargs)
-		self.helper = FormHelper()
-		self.helper.form_action = ""
-		self.helper.form_method = "POST"
-		self.helper.layout = layout.Layout(
-			layout.Fieldset(
-				_("Quote"),
-				layout.Field("author"),
-				layout.Field("quote", rows=3),
-				layout.HTML("""
-					{% include "quotes/includes/image_upload_widget.html" %}
-				"""),
-				layout.Field("picture_path"),  # hidden
-				layout.Field("delete_picture"),  # hidden
-			),
-			bootstrap.FormActions(
-				layout.Submit("submit", _("Save"), css_class="btn btn-primary"),
-			)
-		)
+    def __init__(self, *args, **kwargs):
+        super(InspirationalQuoteForm, self). \
+            __init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_action = ""
+        self.helper.form_method = "POST"
+        self.helper.layout = layout.Layout(
+            layout.Fieldset(
+                _("Quote"),
+                layout.Field("author"),
+                layout.Field("quote", rows=3),
+                layout.HTML("""
+                    {% include "quotes/includes/image_upload_widget.html" %}
+                """),
+                layout.Field("picture_path"),  # hidden
+                layout.Field("delete_picture"),  # hidden
+            ),
+            bootstrap.FormActions(
+                layout.Submit("submit", _("Save"), css_class="btn btn-primary"),
+            )
+        )
 
-	# noinspection PyProtectedMember
-	def save(self, commit=True):
-		instance = super(InspirationalQuoteForm, self). \
-			save(commit=True)
+    # noinspection PyProtectedMember
+    def save(self, commit=True):
+        instance = super(InspirationalQuoteForm, self). \
+            save(commit=True)
 
-		if self.cleaned_data['delete_picture'] and instance.picture:
-			instance.picture.delete()
+        if self.cleaned_data['delete_picture'] and instance.picture:
+            instance.picture.delete()
 
-		if self.cleaned_data['picture_path']:
-			tmp_path = self.cleaned_data['picture_path']
-			abs_tmp_path = os.path.join(settings.MEDIA_ROOT, tmp_path)
+        if self.cleaned_data['picture_path']:
+            tmp_path = self.cleaned_data['picture_path']
+            abs_tmp_path = os.path.join(settings.MEDIA_ROOT, tmp_path)
 
-			filename = InspirationalQuote._meta. \
-				get_field('picture').upload_to(instance, tmp_path)
-			instance.picture.save(
-				filename,
-				File(open(abs_tmp_path, "rb")),
-				False
-			)
-			os.remove(abs_tmp_path)
+            filename = InspirationalQuote._meta. \
+                get_field('picture').upload_to(instance, tmp_path)
+            instance.picture.save(
+                filename,
+                File(open(abs_tmp_path, "rb")),
+                False
+            )
+            os.remove(abs_tmp_path)
 
-		instance.save()
-		return instance
+        instance.save()
+        return instance
