98177959c1efe2dae9c0d54beb996fe03f3721a6
Martin Matovu <matovu.synergy@gmail.com>
2019-03-05 21:47:24 +0300

    registered "Location" model to admin

--- locations/admin.py
+++ locations/admin.py
@@ -1,5 +1,47 @@
+# locations/admin.py
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
+from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
+from django.template.loader import render_to_string
 from .models import Location
 
 
-admin.site.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    save_on_top = True
+    list_display = (
+        "title",
+        "street_address",
+        "description"
+    )
+    search_fields = (
+        "title",
+        "street_address",
+        "description"
+    )
+
+    def get_fieldsets(self, request, obj=None):
+        map_html = render_to_string(
+            "admin/includes/map.html"
+        )
+        fieldsets = [
+            (_("Main Data"), {"fields":
+                                  ("title",
+                                   "description")}
+             ),
+            (_("Address"), {"fields":
+                                ("street_address",
+                                 "village",
+                                 "postal_code",
+                                 "city",
+                                 "country",
+                                 "latitude",
+                                 "longitude")}
+             ),
+            (_("Map"), {"description": map_html, "fields": []}),
+        ]
+        return fieldsets
+
+
+# admin.site.register(Location)
+admin.site.register(Location, LocationAdmin)
