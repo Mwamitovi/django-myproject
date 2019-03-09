28af6c127f04ca4a7a8056efedb51055a8d75816
Martin Matovu <matovu.synergy@gmail.com>
2019-03-09 11:33:14 +0300

    Added more url mappers for more specific views of movies

--- movies/urls.py
+++ movies/urls.py
@@ -2,9 +2,27 @@
 # -*- coding: UTF-8 -*-
 from __future__ import unicode_literals
 from django.conf.urls import url
-from .views import MovieListView
+from django.shortcuts import redirect
+from movies.views import MovieListView
 
 
 urlpatterns = (
     url(r'^$', MovieListView.as_view(), name="movie_list"),
-)
+    # url(r"^$", lambda request: redirect("featured_movie_list")),
+    url(r"^editors-picks/$",
+        "movie_list", {"featured": True},
+        name='featured_movie_list'
+        ),
+    url(r"^commercial/$",
+        "movie_list", {"commercial": True},
+        name="commercial_movie_list"
+        ),
+    url(r"^independent/$",
+        "movie_list", {"independent": True},
+        name="independent_movie_list"
+        ),
+    url(r"^(?P<slug>[^/]+)/$",
+        "movie_detail",
+        name="movie_detail"
+        ),
+    )
