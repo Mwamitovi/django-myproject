065a7eb43954b8bf78f7c74dfea976fafedc8505
Martin Matovu <matovu.synergy@gmail.com>
2019-03-12 12:21:47 +0300

    added Categories to the admin, using customized tree views (DjangoMpttAdmin / TreeEditor)

--- movies/admin.py
+++ movies/admin.py
@@ -1,8 +1,28 @@
+# movies/admin.py
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
 from django.contrib import admin
-from .models import Actor, Director, Genre, Movie
+from django_mptt_admin.admin import DjangoMpttAdmin
+from mptt_tree_editor.admin import TreeEditor
+from .models import Actor, Director, Genre, Movie, Category
 
 
 admin.site.register(Actor)
 admin.site.register(Director)
 admin.site.register(Genre)
 admin.site.register(Movie)
+
+
+# Extending django-mptt-admin
+# class CategoryAdmin(DjangoMpttAdmin):
+#     list_display = ["title", "created", "modified"]
+#     list_filter = ["created"]
+
+
+# Extending django-mptt-tree-editor
+class CategoryAdmin(TreeEditor):
+    list_display = ["indented_short_title", "actions_column", "created", "modified"]
+    list_filter = ["created"]
+
+
+admin.site.register(Category, CategoryAdmin)
