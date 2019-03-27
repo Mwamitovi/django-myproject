eb5dd8ae3ccb400727c88530556d85c82c9f80a0
Martin Matovu <matovu.synergy@gmail.com>
2019-03-27 10:49:29 +0300

    Removed unused "import ugettext"

--- bulletin_board/forms.py
+++ bulletin_board/forms.py
@@ -1,7 +1,7 @@
 # bulletin_board/forms.py
 # -*- coding: UTF-8 -*-
 from django import forms
-from django.utils.translation import ugettext, ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 from crispy_forms.helper import FormHelper
 from crispy_forms import layout, bootstrap
 from .models import TYPE_CHOICES, Category, Bulletin
