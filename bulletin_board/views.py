31f811ff946db0955c89c4a8805efb8c3b730df4
Martin Matovu <matovu.synergy@gmail.com>
2019-03-27 12:24:19 +0300

    Added bulletin list/detail views for rest_framework

--- bulletin_board/views.py
+++ bulletin_board/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# bulletin_board/views.py
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
+from rest_framework import generics
+from .models import Bulletin
+from .serializers import BulletinSerializer
 
-# Create your views here.
+
+class RESTBulletinList(generics.ListCreateAPIView):
+    queryset = Bulletin.objects.all()
+    serializer_class = BulletinSerializer
+
+
+class RESTBulletinDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bulletin.objects.all()
+    serializer_class = BulletinSerializer
