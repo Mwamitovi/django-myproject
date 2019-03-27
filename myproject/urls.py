731e3c24aa0e2de37600f9b35b7fbe2f84f423ab
Martin Matovu <matovu.synergy@gmail.com>
2019-03-27 15:51:17 +0300

    Added urlpattern for "STATIC_ROOT"

--- myproject/urls.py
+++ myproject/urls.py
@@ -96,4 +96,5 @@ urlpatterns = i18n_patterns(
 )
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static( settings.STATIC_URL, document_root=settings.STATIC_ROOT )
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
