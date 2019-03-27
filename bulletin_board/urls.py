527a0bdf0bd6d6aeb2ac28257c96fde1c1a69ff4
Martin Matovu <matovu.synergy@gmail.com>
2019-03-27 10:51:10 +0300

    Added url config for bulletin_board app

---
+++ bulletin_board/urls.py
@@ -0,0 +1,13 @@
+# bulletin_board/urls.py
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
+from django.conf.urls import *
+from .feeds import BulletinFeed
+# from bulletin_board import views
+
+
+urlpatterns = [
+    url(r"^$", "bulletin_list", name="bulletin_list"),
+    url(r"^(?P<bulletin_id>[0-9]+)/$", "bulletin_detail", name="bulletin_detail"),
+    url(r"^rss/$", BulletinFeed(), name="bulletin_rss"),
+]
